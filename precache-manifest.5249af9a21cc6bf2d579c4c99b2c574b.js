b57d469fe54abaa51382bbe3ef5c9fedad972d13
Joni <pacificjoni@gmail.com>
2020-08-30 20:42:50 +0600

    Updates

---
+++ precache-manifest.5249af9a21cc6bf2d579c4c99b2c574b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fbd1528c31302d7906591a545268ea3",
+    "url": "/react_proj_1/index.html"
+  },
+  {
+    "revision": "3fb9ffce87f626dbd2bc",
+    "url": "/react_proj_1/static/js/2.4cb22367.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_proj_1/static/js/2.4cb22367.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef9b9db3d1f7b070fa5d",
+    "url": "/react_proj_1/static/js/main.d6e6ab68.chunk.js"
+  },
+  {
+    "revision": "960d96081af18a850152",
+    "url": "/react_proj_1/static/js/runtime-main.1f691909.js"
+  }
+]);
